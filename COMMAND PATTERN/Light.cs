8fd252dee8e617994f37e041741490165560ebc4
una-07 <esirkepulpan8@gmail.com>
2026-03-06 19:20:39 +0500

    Create Light.cs

---
+++ COMMAND PATTERN/Light.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Light
+{
+    public void On()
+    {
+        Console.WriteLine("Свет включен");
+    }
+
+    public void Off()
+    {
+        Console.WriteLine("Свет выключен");
+    }
+}
