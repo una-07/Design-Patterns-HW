4328caeee73b92eedbc26a074b10d17345797614
una-07 <esirkepulpan8@gmail.com>
2026-03-06 19:41:54 +0500

    Create ChatRoom.cs

---
+++ MEDIATOR/ChatRoom.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+public class ChatRoom : IMediator
+{
+    private List<User> users = new List<User>();
+
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+
+    public void SendMessage(string message, User sender)
+    {
+        foreach (var user in users)
+        {
+            if (user != sender)
+            {
+                user.Receive(message, sender.Name);
+            }
+        }
+    }
+}
