09429b31497f44035f027c35856321537c676190
una-07 <esirkepulpan8@gmail.com>
2026-03-06 19:21:28 +0500

    Create Door.cs

---
+++ COMMAND PATTERN/Door.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Door
+{
+    public void Open()
+    {
+        Console.WriteLine("Дверь открыта");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine("Дверь закрыта");
+    }
+}
