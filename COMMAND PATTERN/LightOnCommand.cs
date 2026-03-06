e1efcc962cf384eabe13d66badc14e6a5f45b195
una-07 <esirkepulpan8@gmail.com>
2026-03-06 19:23:50 +0500

    Create LightOnCommand.cs

---
+++ COMMAND PATTERN/LightOnCommand.cs
@@ -0,0 +1,19 @@
+public class LightOnCommand : ICommand
+{
+    private Light light;
+
+    public LightOnCommand(Light light)
+    {
+        this.light = light;
+    }
+
+    public void Execute()
+    {
+        light.On();
+    }
+
+    public void Undo()
+    {
+        light.Off();
+    }
+}
